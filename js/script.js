cca52f70a8ec28ecae6c748dd4eb6ccfcf05f372
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-09-04 00:47:26 +0300

    Регулярные выражения/Создаем калькулятор на сайте, часть 1 step 20/21

--- js/script.js
+++ js/script.js
@@ -386,6 +386,10 @@ window.addEventListener("DOMContentLoaded", () => {
         dots.push(dot); // пушим в массив точки
     }
 
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     
     function dotsToActive() {
         dots.forEach(dot => dot.style.opacity = '.5');
@@ -402,10 +406,10 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
     next.addEventListener('click', ()=> {
-        if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) { // 500px => 500, если последний слайд
+        if (offset === deleteNotDigits(width) * (slides.length - 1)) { //  если последний слайд
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2); // когда жмём вперед добавляем ширину слайда и он смещается
+            offset += deleteNotDigits(width); // когда жмём вперед добавляем ширину слайда и он смещается
         }
 
         clidesField.style.transform = `translateX(-${offset}px)`;
@@ -423,9 +427,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
     prev.addEventListener('click', ()=> {
         if (offset == 0) { 
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2); 
+            offset -= deleteNotDigits(width); 
         }
 
         clidesField.style.transform = `translateX(-${offset}px)`;
@@ -448,7 +452,7 @@ window.addEventListener("DOMContentLoaded", () => {
         dot.addEventListener('click', (evt)=> { // используем объъект событие
             const slideTo = evt.target.getAttribute('data-slide-to');
             slideIndex = slideTo; // уставили индекс
-            offset = offset = +width.slice(0, width.length - 2) * (slideTo - 1); // установили ширину
+            offset = offset = deleteNotDigits(width) * (slideTo - 1); // установили ширину
 
             clidesField.style.transform = `translateX(-${offset}px)`; // перемещаем слайд по клику
 
@@ -459,5 +463,86 @@ window.addEventListener("DOMContentLoaded", () => {
 
         });
     });
+
+    // Calc
+
+    const result = document.querySelector('.calculating__result span');
+    let sex ='female',
+        height, weight, age,
+        ratio = 1.375;
+
+    // вызываем функцию каждый раз, когда клиент меняет значения
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) { // если не заполен хотябы 1 параметр
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)
+            ) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)
+            ) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`); // получим все элементы
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (evt)=> {// нужен объект события
+                if (evt.target.getAttribute('data-ratio')) {
+                    ratio = +evt.target.getAttribute('data-ratio'); // если пользователь кликнул на умеренную активность , мы взяли и вытащили эту активность которая стояла в дата атрибуте
+                } else {
+                    sex = evt.target.getAttribute('id');
+    
+                }
+    
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                evt.target.classList.add(activeClass);
+    
+                calcTotal();
+            });
+        });
+
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', ()=> { // пользователь что то вводит
+            switch(input.getAttribute('id')) { // каждый раз когда пользователь что-то вводит мы ориентируемся на уникальный индификатор и будет записывать данные в определённую переменную
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+
+            calcTotal();
+        });
+
+        
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+
+
 
 });
