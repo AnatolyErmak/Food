cfd94426e6d1fc076c2b507d563715d7c3d8bf5b
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-09-04 01:56:00 +0300

    Создаем калькулятор на сайте, часть 2 step 22

--- js/script.js
+++ js/script.js
@@ -467,10 +467,42 @@ window.addEventListener("DOMContentLoaded", () => {
     // Calc
 
     const result = document.querySelector('.calculating__result span');
-    let sex ='female',
-        height, weight, age,
-        ratio = 1.375;
 
+    let sex ,height, weight, age, ratio ;
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
+
+    function initLocalSetting(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            if (elem.getAttribute('id')=== localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            }
+
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+ 
+    initLocalSetting('#gender div', 'calculating__choose-item_active');
+    initLocalSetting('.calculating__choose_big div', 'calculating__choose-item_active');
+    
+    
     // вызываем функцию каждый раз, когда клиент меняет значения
 
     function calcTotal() {
@@ -478,7 +510,7 @@ window.addEventListener("DOMContentLoaded", () => {
             result.textContent = '____';
             return;
         }
-
+ 
         if (sex === 'female') {
             result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)
             ) * ratio);
@@ -490,15 +522,17 @@ window.addEventListener("DOMContentLoaded", () => {
 
     calcTotal();
 
-    function getStaticInformation(parentSelector, activeClass) {
-        const elements = document.querySelectorAll(`${parentSelector} div`); // получим все элементы
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(selector); // получим все элементы
 
         elements.forEach(elem => {
             elem.addEventListener('click', (evt)=> {// нужен объект события
                 if (evt.target.getAttribute('data-ratio')) {
                     ratio = +evt.target.getAttribute('data-ratio'); // если пользователь кликнул на умеренную активность , мы взяли и вытащили эту активность которая стояла в дата атрибуте
+                    localStorage.setItem('ratio', +evt.target.getAttribute('data-ratio'));
                 } else {
                     sex = evt.target.getAttribute('id');
+                    localStorage.setItem('sex', evt.target.getAttribute('id'));
     
                 }
     
@@ -514,13 +548,20 @@ window.addEventListener("DOMContentLoaded", () => {
 
     }
 
-    getStaticInformation('#gender', 'calculating__choose-item_active');
-    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
     function getDynamicInformation(selector) {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', ()=> { // пользователь что то вводит
+
+            if(input.value.match(/\D/g)) {
+                input.style.border = '1px solid red';
+            } else {
+                input.style.border = 'none';
+            }
+
             switch(input.getAttribute('id')) { // каждый раз когда пользователь что-то вводит мы ориентируемся на уникальный индификатор и будет записывать данные в определённую переменную
                 case 'height':
                     height = +input.value;
